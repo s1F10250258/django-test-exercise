ea688a52711c95f7eaad8aebae594e04236bb4a5
1F10250258 <s1F102502586@iniad.org>
2026-06-25 13:23:13 +0900

    テストのサンプルを追加

--- todo/tests.py
+++ todo/tests.py
@@ -5,7 +5,7 @@ from todo.models import Task
 
 # Create your tests here.
 class SampleTestCase(TestCase):
-    def test_sample(self):
+    def test_sample1(self):
         self.assertEqual(1 + 2, 3)
 
 
